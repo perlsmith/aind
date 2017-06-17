fe2c1588b9dd74ceca4d504c5f6b6dcb250d4f94
Ananth Chellappa <ananth.ch@gmail.com>
2017-06-17 00:10:10 -0400

    eliminate okay

--- sudoku.py
+++ sudoku.py
@@ -28,18 +28,30 @@ puzzle = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3.
 
 def eliminate( grid ) :
 
-    """Eliminate values from peers of each box with a single value.
+	"""Eliminate values from peers of each box with a single value.
 
-    Go through all the boxes, and whenever there is a box with a single value,
-    eliminate this value from the set of values of all its peers.
+	Go through all the boxes, and whenever there is a box with a single value,
+	eliminate this value from the set of values of all its peers.
 
-    Args:
-        values: Sudoku in dictionary form.
-    Returns:
-        Resulting Sudoku in dictionary form after eliminating values.
-    """
+	Args:
+	values: Sudoku in dictionary form.
+	Returns:
+	Resulting Sudoku in dictionary form after eliminating values.
+	"""
+
 	for box in boxes :
 		if 1 == len( grid[box] ) :	# then, need to hit the peers - all 20 of them...
+			# print( box )
+			for unit in unitlist :
+				if box in unit :
+					# pdb.set_trace()
+					for cell in unit :
+						if not cell == box :
+							grid[cell] = re.sub( grid[box] , '' , grid[cell] )
+	return grid
+	
+sudoku = grid_values( puzzle )
+su = eliminate( sudoku )
 	
 display( eliminate( grid_values( puzzle ) ) )
 	
